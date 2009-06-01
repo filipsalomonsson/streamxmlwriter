d180c9eaac50d0ebe3f655836f2d73796fc658b2
Filip Salomonsson <filip.salomonsson@gmail.com>
2009-06-01 23:08:52 +0200

    Added much needed docstrings and comments.

--- streamxmlwriter.py
+++ streamxmlwriter.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-"""streamxmlwriter - A simple library for incrementally writing XML
+"""
+streamxmlwriter - A simple library for incrementally writing XML
 files of arbitrary size. Supports pretty-printing and custom attribute
 ordering. Experimental namespace support.
 
-In early development; poor documentation and tests; may eat your
-children. The latest development version is available from the git
-repository: http://github.com/infixfilip/streamxmlwriter/tree/master
+In development; poor documentation and tests; may eat your children.
+The latest development version is available from the git repository [1].
+
+[1] http://github.com/infixfilip/streamxmlwriter/tree/master
 
 Comments and/or patches are always welcome.
 """
@@ -37,6 +39,7 @@ INDENT = "  "
 
 
 def escape_attribute(value, encoding):
+    """Escape an attribute value using the given encoding."""
     if "&" in value:
         value = value.replace("&", "&amp;")
     if "<" in value:
@@ -47,6 +50,7 @@ def escape_attribute(value, encoding):
 
 
 def escape_cdata(data, encoding):
+    """Escape character data using the given encoding."""
     if "&" in data:
         data = data.replace("&", "&amp;")
     if "<" in data:
@@ -57,13 +61,23 @@ def escape_cdata(data, encoding):
 
 
 def sorter_factory(attrib_order):
+    """Return a function that sorts a list of (key, value) pairs.
+
+    The sort order is determined by the `attrib_order` dictionary,
+    whose format is described in the documentation for the `XMLWriter`
+    class.
+
+    """
     for tag, names in attrib_order.iteritems():
         attrib_order[tag] = dict((name, n) for (n, name) in enumerate(names))
     for tag, order in attrib_order.iteritems():
         order[None] = len(order)
 
     def asort(pairs, tag):
+        """Sort a list of ``(key, value)`` pairs), using the custom
+        sort order for the given `tag` name."""
         def key(a):
+            # Sort key
             name, value = a
             if tag not in attrib_order:
                 return name
@@ -77,6 +91,13 @@ def sorter_factory(attrib_order):
 
 
 def tostring(element, *args, **kwargs):
+    """Serialize an element to its string representation using an
+    `XMLWriter`.
+
+    `element` is an Element instance. All additional positional and
+    keyword arguments are passed on to the underlying `XMLWriter`.
+
+    """
     import cStringIO
     out = cStringIO.StringIO()
     writer = XMLWriter(out, *args, **kwargs)
@@ -86,7 +107,7 @@ def tostring(element, *args, **kwargs):
 
 
 class XMLSyntaxError(Exception):
-    pass
+    """XML syntactic errors, such as ill-nestedness."""
 
 
 class XMLWriter(object):
@@ -94,6 +115,31 @@ class XMLWriter(object):
     def __init__(self, file, encoding="utf-8",
                  nsmap={}, default_namespace=None,
                  pretty_print=False, sort=True):
+        """
+        Create an `XMLWriter` that writes its output to `file`.
+
+        `encoding` is the output encoding (default: utf-8). If
+        `pretty_print` is true, the output will be written in indented.
+        form.
+
+        If `sort` is true (which is the default), attributes will be
+        sorted alphabetically.
+
+        Optionally, `sort` can be a dictionary specifying a custom
+        sort order for attributes. The dictionary keys are tag names,
+        and each value is a list of attribute names in the order they
+        should appear when sorted. If `None` appears in the list, it
+        acts as a wildcard for all attributes not explicitly named in
+        the list. (By default, they will be placed last.)
+
+        Example::
+
+        attrib_order = {
+            "person": ["id", "first_name", "last_name"],
+            "foo": ["id", None, "put_me_last"],
+        }
+
+        """
         self.write = file.write
         self.encoding = encoding
         self._pretty_print = pretty_print
@@ -111,6 +157,7 @@ class XMLWriter(object):
             self.declaration()
 
     def _cname(self, name):
+        # Return a cname from its {ns}tag form
         if name[0] == "{":
             uri, name = name[1:].split("}", 1)
             nsmap = self._namespace_maps[-1]
@@ -124,16 +171,25 @@ class XMLWriter(object):
         return name
 
     def start(self, tag, attributes=None, nsmap={}, **kwargs):
+        """Open a new `tag` element.
+
+        Attributes can be given as a dictionary (`attributes`), or as
+        keyword arguments.
+
+        """
         if self._start_tag_open:
             self.write(">")
             self._start_tag_open = False
         if self._pretty_print and self._tags and not self._wrote_data:
             self.write("\n" + INDENT * len(self._tags))
+        # Generate start-ns events for all new namespaces
         for (prefix, uri) in nsmap.iteritems():
             if self._namespace_maps[-1].get(uri) != prefix:
                 self.start_ns(prefix, uri)
         tag = self._cname(tag)
         self.write("<" + tag)
+        # Write attributes, including namespace declarations
+        # TODO: Handle ns declarations separately
         if attributes or kwargs or self._new_namespaces:
             if attributes is None:
                 attributes={}
@@ -146,11 +202,13 @@ class XMLWriter(object):
                     attributes["xmlns"] = uri
             self._new_namespaces = []
             attributes = attributes.items() + kwargs.items()
+            # Sort them, if requested
             if self._sort:
                 if callable(self._sort):
                     attributes = self._sort(attributes, tag)
                 else:
                     attributes = sorted(attributes)
+            # Do the actual writing
             for name, value in attributes:
                 name = self._cname(name)
                 self.write(" " + name + "=\""
@@ -161,12 +219,20 @@ class XMLWriter(object):
         self._tags.append(tag)
 
     def end(self, tag=None):
+        """Close the most recently opened element.
+
+        If `tag` is given, it must match the tag name of the open
+        element, or an `XMLSyntaxError will be raised.
+
+        """
         open_tag = self._tags.pop()
         if tag is not None:
             tag = self._cname(tag)
             if open_tag != tag:
                 raise XMLSyntaxError("Start and end tag mismatch: %s and /%s."
                                      % (open_tag, tag))
+        # Use the short form for empty elements
+        # TODO: make this configurable
         if self._start_tag_open:
             self.write(" />")
             self._start_tag_open = False
@@ -177,15 +243,20 @@ class XMLWriter(object):
         self._wrote_data = False
 
     def start_ns(self, prefix, uri):
+        """Add a namespace declaration to the scope of the next
+        element."""
         new_map = self._namespace_maps[-1].copy()
         new_map[uri] = prefix
         self._namespace_maps.append(new_map)
         self._new_namespaces.append((uri, prefix))
 
     def end_ns(self):
+        """End a namespace scope."""
+        # TODO: remove this, as it should be handled automatically
         del self._namespace_maps[-1]
 
     def data(self, data):
+        """Add character data."""
         if not (self._pretty_print and not data.strip()):
             if self._start_tag_open:
                 self.write(">")
@@ -219,22 +290,27 @@ class XMLWriter(object):
             self.end(element)
 
     def _close_start(self):
+        # Make sure the start tag is finished
         if self._start_tag_open:
             self.write(">")
         self._start_tag_open = False
 
     def declaration(self):
+        """Write an XML declaration."""
         self.write("<?xml version='1.0' encoding='" + self.encoding + "'?>")
     xml = declaration
 
     def comment(self, data):
+        """Add an XML comment."""
         self._close_start()
         self.write("<!--" + escape_cdata(data, self.encoding) + "-->")
 
     def pi(self, target, data):
+        """Add an XML processing instruction."""
         self._close_start()
         self.write("<?" + target + " " + data + "?>")
 
     def close(self):
+        """Close all open elements."""
         while self._tags:
             self.end()
